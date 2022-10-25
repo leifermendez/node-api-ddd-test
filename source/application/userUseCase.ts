19c559eec0ca616e81d16d841fc176a76a85c32e
Carlos Díaz <ernesto.diaz@finakiva.com>
2022-10-25 11:22:11 -0500

    Ajuste con la entidad principal

--- source/application/userUseCase.ts
+++ source/application/userUseCase.ts
@@ -1,11 +1,11 @@
 import { UserRepository } from "../domain/models/user/UserRepository";
-import { UserValue } from "../domain/user.value";
+import {User} from "../domain/models/user/User";
 
 export class UserUseCase {
   constructor(private readonly userRepository: UserRepository) {}
 
-  public  registerUser = async ({ name, email, description }) => {
-    const userValue = new UserValue({ name, email });
+  public  registerUser = async ( {name, email, description} ) => {
+    const userValue = new User(name, email,description );
     const userCreated = await this.userRepository.registerUser(userValue);
     return userCreated
   }
