0f725620d3a91378e05c42775a58420a15f43a68
Carlos Díaz <ernesto.diaz@finakiva.com>
2022-10-25 11:09:56 -0500

    Organizando proyecto en carpetas

---
+++ source/domain/models/user/UserRepository.ts
@@ -0,0 +1,7 @@
+import { User } from "./models/user/User";
+
+export interface UserRepository {
+  findUserById(uuid: string): Promise<User | null>;
+  registerUser(user:User): Promise<User | null>;
+  listUser(): Promise<User[] | null>;
+}
--- source/domain/user.repository.ts
+++
@@ -1,7 +0,0 @@
-import { UserEntity } from "./user.entity";
-
-export interface UserRepository {
-  findUserById(uuid: string): Promise<UserEntity | null>;
-  registerUser(user:UserEntity): Promise<UserEntity | null>;
-  listUser(): Promise<UserEntity[] | null>;
-}
