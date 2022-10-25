fe50f71be1af757a731c5b32c89a5d604ab0c486
Carlos Díaz <ernesto.diaz@finakiva.com>
2022-10-25 13:25:32 -0500

    ajustes para conexion

--- source/infrastructure/db/mongo.ts
+++ source/infrastructure/db/mongo.ts
@@ -3,8 +3,15 @@ import { connect } from "mongoose";
 const DB_URI = `${process.env.DB_URI}`
 
 const dbInit = async () => {
-    await connect(DB_URI)
-    console.log('Estamos ready?')
+    try {
+        console.log("Conectando a mongo")
+        const result = await connect(DB_URI)
+        console.log(result)
+        console.log('Solo mondongo')
+    }
+    catch (e) {
+        console.log("Fallo al conectar a mongo")
+    }
 }
 
 export default dbInit
