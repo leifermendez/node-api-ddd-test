ff3c8c27079f75e1935c63928c821982f85e7e6b
Carlos Díaz <ernesto.diaz@finaktiva.com>
2022-10-26 08:58:37 -0500

    Mejorando  codigo

--- source/infrastructure/db/mongo.ts
+++ source/infrastructure/db/mongo.ts
@@ -1,16 +1,16 @@
 import { connect } from "mongoose";
 
-const DB_URI = `${process.env.DB_URI}`
+const DB_URI = `${process.env.DB_URI}`;
 
 const dbInit = async () => {
-    try {
-        console.log("Conectando a mongo")
-        await connect(DB_URI)
-        console.log('Solo mollejadb')
-    }
-    catch (e) {
-        console.log("Fallo al conectar a mongo")
-    }
-}
+  try {
+    console.log("Conectando a mongo");
+    await connect(DB_URI);
+    console.log("Solo mollejadb");
+  } catch (e) {
+    console.log("error:", e);
+    console.log("Fallo al conectar a mongo");
+  }
+};
 
-export default dbInit
+export default dbInit;
