b140011caa3086a2bf0a4552ce7a01eb9d2416f0
Carlos Díaz <ernesto.diaz@finaktiva.com>
2022-10-28 16:24:47 -0500

    Combinando casos de uso en una tarea programada

--- src/infrastructure/scheduler/user/UserScheduler.ts
+++ src/infrastructure/scheduler/user/UserScheduler.ts
@@ -2,13 +2,22 @@ import { SchedulerConfig } from "../SchedulerConfig";
 import { PostgresSQLRepository } from "../../repository/PostgreSQLRepository";
 import { UserEntity } from "../../data/UserEntity";
 import { UserCrudUseCase } from "../../../domain/usecase/user/UserCrudUseCase";
+import { v4 as uuid } from "uuid";
 
 const repositoryPostgresSQL = new PostgresSQLRepository(UserEntity);
 const useCasePostgresSQL = new UserCrudUseCase(repositoryPostgresSQL);
 
 export const schedulerGetAllUsers = async () => {
-  const scheduler: SchedulerConfig = new SchedulerConfig("* * * * *", () =>
-    useCasePostgresSQL.getAllUsers()
+  const scheduler: SchedulerConfig = new SchedulerConfig(
+    "* * * * * *",
+    async () => {
+      const data: any = {
+        name: "lo que sea",
+        email: uuid().concat("@mail.com"),
+      };
+      await useCasePostgresSQL.registerUser(data);
+      return useCasePostgresSQL.getAllUsers();
+    }
   );
   await scheduler.start();
 };
