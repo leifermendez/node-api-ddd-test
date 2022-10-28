2634a0cfe9852212f164c98fd7da43e21974678b
Carlos Díaz <ernesto.diaz@finaktiva.com>
2022-10-28 16:30:57 -0500

    Ajustando para dejar el ejemplo sencillo demostrativo

--- src/infrastructure/scheduler/user/UserScheduler.ts
+++ src/infrastructure/scheduler/user/UserScheduler.ts
@@ -2,22 +2,14 @@ import { SchedulerConfig } from "../SchedulerConfig";
 import { PostgresSQLRepository } from "../../repository/PostgreSQLRepository";
 import { UserEntity } from "../../data/UserEntity";
 import { UserCrudUseCase } from "../../../domain/usecase/user/UserCrudUseCase";
-import { v4 as uuid } from "uuid";
 
 const repositoryPostgresSQL = new PostgresSQLRepository(UserEntity);
 const useCasePostgresSQL = new UserCrudUseCase(repositoryPostgresSQL);
 
 export const schedulerGetAllUsers = async () => {
   const scheduler: SchedulerConfig = new SchedulerConfig(
-    "* * * * * *",
-    async () => {
-      const data: any = {
-        name: "lo que sea",
-        email: uuid().concat("@mail.com"),
-      };
-      await useCasePostgresSQL.registerUser(data);
-      return useCasePostgresSQL.getAllUsers();
-    }
+    "5 0 1 * *", //At 00:05 on day-of-month 1
+    async () => useCasePostgresSQL.getAllUsers()
   );
   await scheduler.start();
 };
